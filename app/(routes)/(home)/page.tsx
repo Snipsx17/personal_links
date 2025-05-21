0947a40f2e9180ee590e7c27e910abb9ed6e6e77
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-05-21 15:06:29 -0400

    Feat: implement states and context on home page

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,7 +1,49 @@
+"use client";
+
 import { TreePalm } from "lucide-react";
 import { LinkProfile } from "./Components";
+import { useUser } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+import { Link, User } from "@/lib/generated/prisma";
+import { LoaderProfile } from "@/components/shared";
+import { StepConfigUserProvider } from "@/context";
 
 export default function HomePage() {
+  const { user } = useUser();
+  const [isFirstVisit, setIsFirstVisit] = useState<boolean>(true);
+  const [reload, setReload] = useState<boolean>(false);
+  const [userInfo, setUserInfo] = useState<(User & { links: Link[] }) | null>(
+    null,
+  );
+
+  useEffect(() => {
+    const checkFirstLogin = async () => {
+      const response = await fetch("/api/info_user");
+      const data: User & { links: Link[] } = await response.json();
+      setUserInfo(data);
+      setIsFirstVisit(data.firstLogin);
+    };
+
+    checkFirstLogin();
+
+    if (reload) {
+      checkFirstLogin();
+      setReload(false);
+    }
+  }, [user?.id, user, reload]);
+
+  if (!user || !userInfo) {
+    return <LoaderProfile />;
+  }
+
+  if (isFirstVisit) {
+    return (
+      <StepConfigUserProvider>
+        <p>Is the first visit...</p>
+      </StepConfigUserProvider>
+    );
+  }
+
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-[60%_auto] gap-4 px-4">
